590f62cb0128189d7387f6463fba3c33746d548e
annabelcowley <annabel.cowley23@gmail.com>
2020-04-26 17:14:47 +1000

    developing the consultation request activity

--- app/src/main/java/com/example/chatapp/RequestConsultationActivity.java
+++ app/src/main/java/com/example/chatapp/RequestConsultationActivity.java
@@ -7,6 +7,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -22,7 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class RequestConsultationActivity extends AppCompatActivity {
+public class RequestConsultationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner areaSpinner, clinicSpinner, specialitySpinner, doctorSpinner;
     ImageButton back_btn;
@@ -30,60 +31,86 @@ public class RequestConsultationActivity extends AppCompatActivity {
 
     FirebaseUser fuser;
     DatabaseReference reference;
+    private AdapterView<?> arg0;
+    private View arg1;
+    private int arg2;
+    private long arg3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_consultation);
+
+        areaSpinner = (Spinner) findViewById(R.id.areaSpinner);
+        clinicSpinner = (Spinner) findViewById(R.id.clinicSpinner);
+        specialitySpinner = (Spinner) findViewById(R.id.specialitySpinner);
+        doctorSpinner = (Spinner) findViewById(R.id.doctorSpinner);
+
+        areaSpinner.setOnItemSelectedListener(this);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Request Consultation");
 
-        addItemsOnClinicSpinner();
-
-        addListenerOnButton();
-
-        addListenerOnSpinnerItemSelection();
-
     }
 
-    // get the selected dropdown list value
-    public void addListenerOnSpinnerItemSelection() {
-
-        areaSpinner = (Spinner) findViewById(R.id.areaSpinner);
-
-        areaSpinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-
-    }
-
-
-
-    // add items into clinic spinner dynamically
-    public void addItemsOnClinicSpinner() {
-
-        clinicSpinner = (Spinner) findViewById(R.id.clinicSpinner);
-
-        List<String> list = new ArrayList<String>();
-
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View arg1, int arg2, long arg3) {
+        String spArea = String.valueOf(areaSpinner.getSelectedItem());
+        Toast.makeText(this, spArea, Toast.LENGTH_SHORT).show();
+        if(spArea.contentEquals("Sydney CBD")) {
+            List<String> list = new ArrayList<String>();
             list.add("Barangaroo Clinic, 200 Barangaroo Avenue");
-
             list.add("Chippendale Clinic, 29 Broardway Street");
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, list);
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter.notifyDataSetChanged();
+            clinicSpinner.setAdapter(dataAdapter);
+        }
+        if(spArea.contentEquals("Western Sydney")) {
+            List<String> list = new ArrayList<String>();
+            list.add("Lakemba Clinic, 27 Railway Parade");
+            list.add("Cabramatta Clinic, 117 John Street");
+            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, list);
+            dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter2.notifyDataSetChanged();
+            clinicSpinner.setAdapter(dataAdapter2);
+        }
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+        String spClinic = String.valueOf(clinicSpinner.getSelectedItem());
 
-                android.R.layout.simple_spinner_item, list);
+        String spSpeciality = String.valueOf(specialitySpinner.getSelectedItem());
+        Toast.makeText(this, spArea, Toast.LENGTH_SHORT).show();
+        if(spSpeciality.contentEquals("General") ) {
+            List<String> list = new ArrayList<String>();
+            list.add("Dr Sam Smith");
+            list.add("Dr Alexandra Jones");
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, list);
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter.notifyDataSetChanged();
+            doctorSpinner.setAdapter(dataAdapter);
+        }
+        if(spSpeciality.contentEquals("Pediatrics")) {
+            List<String> list = new ArrayList<String>();
+            list.add("Dr John Johnston");
+            list.add("Dr Adelaide White");
+            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, list);
+            dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter2.notifyDataSetChanged();
+            doctorSpinner.setAdapter(dataAdapter2);
+        }
 
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        String spDoctor = String.valueOf(doctorSpinner.getSelectedItem());
 
-        clinicSpinner.setAdapter(dataAdapter);
 
     }
 
-
-
-
-    public void addListenerOnButton() {
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
