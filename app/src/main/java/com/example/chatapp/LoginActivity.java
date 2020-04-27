1a72b114ab5809029278213ea42be04815fe7196
islam-cmd <56726673+islam-cmd@users.noreply.github.com>
2020-04-27 10:45:58 +1000

    PatientDashboard activity

--- app/src/main/java/com/example/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -79,7 +79,7 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, PatientDashboard.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                 startActivity(intent);
                                 finish();
