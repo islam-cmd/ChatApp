4843885acdfaee1f5df3d70f96119104690c4983
ClodaghMM <62277898+ClodaghMM@users.noreply.github.com>
2020-05-10 20:36:12 +1000

    Update Viewing_Patients_activity.java
    Just finished coding this. Having fully tested whether the patients appear in the doctor's screen yet. But it's managed to run and shows the empty list.

--- app/src/main/java/com/example/chatapp/Viewing_Patients_activity.java
+++ app/src/main/java/com/example/chatapp/Viewing_Patients_activity.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.example.chatapp.Model.User;
+import com.example.chatapp.Model.View_Patient;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,9 +32,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Viewing_Patients_activity extends AppCompatActivity {
-    
-    //IMPORTANT NOTE
-//I am still experiencing errors with this. 
 
 
     Button viewPatHistoryBtn;
@@ -42,6 +40,8 @@ public class Viewing_Patients_activity extends AppCompatActivity {
     DatabaseReference reference;
     Button dashboard_backbtn;
     ViewPager viewpat;
+    Toolbar patToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,26 +50,24 @@ public class Viewing_Patients_activity extends AppCompatActivity {
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
         viewpat = findViewById(R.id.pat_view);
-        Toolbar patToolbar = findViewById(R.id.patient_bar);
+        //atToolbar = findViewById(R.id.patient_bar);
         TabLayout docTab = findViewById(R.id.tab_patients);
         reference.addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
-                assert user != null;
-                username.setText(user.getUsername());
+                View_Patient patient = dataSnapshot.getValue(View_Patient.class);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
 
-        DocMainActivity.ViewPagerAdapter viewPagerAdapter = new DocMainActivity.ViewPagerAdapter(getSupportFragmentManager());
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewpat.setAdapter(viewPagerAdapter);
         docTab.setupWithViewPager(viewpat);
 
+        dashboard_backbtn = findViewById(R.id.dashboard_back);
         dashboard_backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,6 +80,41 @@ public class Viewing_Patients_activity extends AppCompatActivity {
     }
 
 }
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> patientFragments;
+    private ArrayList<String> patientNames;
+
+
+    ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        this.patientFragments = new ArrayList<>();
+        this.patientNames = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return patientFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return patientFragments.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        patientFragments.add(fragment);
+        patientNames.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return patientNames.get(position);
+    }
+}
+
 
 
 
