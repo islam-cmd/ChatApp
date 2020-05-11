66dd8f5dd3db79d2077049778391691beb35f0ac
ClodaghMM <62277898+ClodaghMM@users.noreply.github.com>
2020-05-11 20:15:17 +1000

    Create ViewPatientList.java

---
+++ app/src/main/java/com/example/chatapp/Model/ViewPatientList.java
@@ -0,0 +1,79 @@
+package com.example.chatapp.Model;
+
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+
+import java.util.List;
+
+public class ViewPatientList implements ListAdapter {
+    private Activity context;
+    public List<Patient> viewAppointments;
+
+    public ViewPatientList(Activity context, List<Patient> viewAppointments) {
+        this.context = context;
+        this.viewAppointments = viewAppointments;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int i) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
