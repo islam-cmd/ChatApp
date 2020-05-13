78786488a830795c48d313f0e6253b56a66a7010
Albert-13649203 <62406883+Albert-13649203@users.noreply.github.com>
2020-05-13 17:31:32 +1000

    .

--- app/src/main/java/com/example/chatapp/PatientDashboard.java
+++ app/src/main/java/com/example/chatapp/PatientDashboard.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.nio.file.Path;
+
 
 
 public class PatientDashboard extends AppCompatActivity {
@@ -18,7 +20,6 @@ public class PatientDashboard extends AppCompatActivity {
     Button contactSupport_btn;
 
 
-import java.nio.file.Path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,5 +53,6 @@ import java.nio.file.Path;
                 finish();
             }
         }));
+
     }
 }
