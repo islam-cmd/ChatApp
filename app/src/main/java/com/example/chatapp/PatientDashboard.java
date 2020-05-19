238df8fe423d04825be1d85c9206fa09c1e3b425
annabelcowley <annabel.cowley23@gmail.com>
2020-05-19 22:10:31 +1000

    messing around with request consultation

--- app/src/main/java/com/example/chatapp/PatientDashboard.java
+++ app/src/main/java/com/example/chatapp/PatientDashboard.java
@@ -16,7 +16,9 @@ public class PatientDashboard extends AppCompatActivity {
     Button view_doctors_btn;
     Button view_appointment_btn;
     Button contactSupport_btn;
-    Button emergency_btn; 
+    Button view_profile_btn;
+    Button emergency_btn;
+Button OnlineConsultation;
 
 
 
@@ -27,8 +29,10 @@ public class PatientDashboard extends AppCompatActivity {
         viewMes_btn = findViewById(R.id.ChatActivity);
         logout_btn = findViewById(R.id.log_out);
         view_doctors_btn = findViewById(R.id.view_doc);
+        view_profile_btn= findViewById(R.id.view_profile);
+
         emergency_btn = findViewById(R.id.Urgent_Case);
-        
+OnlineConsultation = findViewById(R.id.OnlineConsultation);
 
         viewMes_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,9 +45,9 @@ public class PatientDashboard extends AppCompatActivity {
         schedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               //Intent intent = new Intent(PatientDashboard.this, RequestConsultationActivity.class);
-                //startActivity(intent);
-                finish();
+               Intent intent = new Intent(PatientDashboard.this, Request_ConsultationActivity.class);
+                startActivity(intent);
+              finish();
             }
         });
         emergency_btn.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +66,24 @@ public class PatientDashboard extends AppCompatActivity {
                 finish();
             }
         }));
+        view_profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientDashboard.this,UserInfoDisplay.class);
+                startActivity(intent);
 
+            }
+        });
+
+
+OnlineConsultation.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(PatientDashboard.this, OnlineConsultationActivity.class);
+        startActivity(intent);
+
+    }
+});
         //view_appointment_btn.setOnClickListener((new View.OnClickListener() {
             //@Override
           //  public void onClick(View v) {
